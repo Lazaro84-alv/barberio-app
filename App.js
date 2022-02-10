bb330e368b8c38b5df6de26612892d64029496e7
Lázaro Alves <geo.eolo@gmail.com>
2022-02-10 17:29:19 -0300

    Projeto Base

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+
+import MainStack from './src/stacks/MainStack'
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <MainStack />
+    </NavigationContainer>
+  )
+}
