5bbdb5baf9edc8c6b8e826a7682ffbb903a1caf6
Lázaro Alves <geo.eolo@gmail.com>
2022-02-11 15:42:05 -0300

    Adiciona UserReducer

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -4,10 +4,10 @@ import { initialState, UserReducer } from '../reducers/UserReducer'
 export const UserContext = createContext()
 
 export default ({children}) =>  {
-    
+    const [state, dispatch] = useReducer(UserReducer, initialState)
     
     return (
-        <UserContext.Provider>
+        <UserContext.Provider value={{state, dispatch}}>
             {children}
         </UserContext.Provider>
     )
