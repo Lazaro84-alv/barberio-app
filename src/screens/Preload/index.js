2f2705070089c571904ba781bcee73bfa791a72e
Lázaro Alves <geo.eolo@gmail.com>
2022-02-11 15:27:34 -0300

    Cria UserContext

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, LoadingIcon } from './styles'
+import { AsyncStorage } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
 
 import BarberLogo from '../../assets/barber.svg'
 
 export default () => {
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        const checkToken = async () => {
+            const token = await AsyncStorage.getItem('token')
+            if(token !== null) {
+                // validar o token
+            } else {
+                navigation.navigate('SignIn')
+            }
+        }
+        checkToken()
+    }, [])
+
     return (
         <Container>
             <BarberLogo width="100%" height="160" />
