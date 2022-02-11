b5734e4b58dc66223591eb32e712ea682c2d2695
Lázaro Alves <geo.eolo@gmail.com>
2022-02-11 10:07:38 -0300

    Adiciona os assets

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import Preload from '../screens/Preload'
+import SignIn from '../screens/SignIn'
+import SingUp from '../screens/SignUp'
+
 const Stack = createNativeStackNavigator()
 
 export default () => {
-    <Stack.Navigator>
-        <Stack.Screen name="Preload" component={Preload} />
-        <Stack.Screen name="Preload" component={SignIn} />
-        <Stack.Screen name="Preload" component={SingUp} />
-    </Stack.Navigator>
+    return (
+        <Stack.Navigator
+            initialRouteName="Preload"
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Stack.Screen name="Preload" component={Preload} />
+            <Stack.Screen name="SignIn" component={SignIn} />
+            <Stack.Screen name="SignUp" component={SingUp} />
+        </Stack.Navigator>
+    )
 }
