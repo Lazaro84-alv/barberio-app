04730e9670d00c17634391ff0a64d53b15974e1f
Lázaro Alves <geo.eolo@gmail.com>
2022-02-14 13:35:11 -0300

    Adiciona o refresh

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Platform } from 'react-native'
+import { Platform, RefreshControl } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { request, PERMISSIONS } from 'react-native-permissions'
 import Geolocation from '@react-native-community/geolocation'
@@ -34,6 +34,7 @@ export default () => {
     const [coords, setCoords] = useState(null)
     const [loading, setLoading] = useState(false)
     const [list, setList] = useState([])
+    const [refreshing, setRefreshing] = useState(false)
 
     const handleLocationFinder = async () => {
         setCoords(null)
@@ -61,7 +62,14 @@ export default () => {
         setLoading(true)
         setList([])
 
-        let res = await Api.getBarbers()
+        let lat = null
+        let lng = null
+        if(coords) {
+            lat = coords.latitude
+            lng = coords.longitude
+        }
+
+        let res = await Api.getBarbers(lat, lng, locationText)
         if(res.error == '') {
             if(res.loc) {
                 setLocationText(res.loc)
@@ -78,9 +86,21 @@ export default () => {
         getBarbers()
     }, [])
 
+    const onRefresh = () => {
+        setRefreshing(false)
+        getBarbers()
+    }
+
+    const handleLocationSearch = () => {
+        setCoords({})
+        getBarbers()
+    }
+
     return (
         <Container>
-            <Scroller>
+            <Scroller refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }>
 
                 <HeaderArea>
                     <HeaderTitle numberOfLines={2}>Encontre o seu barbeiro favorito</HeaderTitle>
@@ -95,6 +115,7 @@ export default () => {
                         placeholderTextColor="#FFFFFF"
                         value={locationText}
                         onChangeText={t=>setLocationText(t)}
+                        onEndEditing={handleLocationSearch}
                     />
                     <LocationFinder onPress={handleLocationFinder}>
                         <MyLocationIcon width="24" height="24" fill="#FFFFFF" />
